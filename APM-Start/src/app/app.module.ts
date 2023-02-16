83677ba706024abe900d7f7997646b8a9e4a59a6
Luis Ignacio Mateo <33287751+LuisIgnacio98@users.noreply.github.com>
2023-02-16 08:50:17 -0400

    Modulo 6 Completado

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
@@ -10,10 +11,11 @@ import { ProductListComponent } from './products/product-list.component';
         AppComponent,
         ProductListComponent
     ],
-    bootstrap: [AppComponent],
     imports: [
-        BrowserModule
+        BrowserModule,
+        FormsModule
         
-    ]
+    ],
+    bootstrap: [AppComponent],
 })
 export class AppModule { }
